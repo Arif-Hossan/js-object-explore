684bcf9c60deaf83fc47908b2ed641202b71ee0b
Arif Hossan <arif.hossan.eu@gmail.com>
2023-02-11 02:39:05 +0600

    not recomended way of cleaning object

--- copy.js
+++ copy.js
@@ -9,3 +9,7 @@ console.log(data2);
 // deep copy
 const data3 = JSON.parse(JSON.stringify(data));
 console.log(data3);
+
+//don't use because it refer the same address
+const notRecomendedWay=data;
+console.log(notRecomendedWay);
